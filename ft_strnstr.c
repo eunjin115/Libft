1c51d0ca7752f8dd9c5a78a5621cfd835bf9485c
eunjin115 <katherine0115@naver.com>
2020-12-23 10:04:22 +0900

    MOD ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: eunjikim <eunjikim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 23:32:18 by eunjikim          #+#    #+#             */
-/*   Updated: 2020/12/23 09:57:26 by eunjikim         ###   ########.fr       */
+/*   Updated: 2020/12/23 10:02:02 by eunjikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char *	ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (*needle == '\0' || len == 0)
+	if (*needle == '\0')
 		return ((char *)haystack);
 	i = 0;
 	while (i < len && haystack[i] != '\0')
@@ -25,9 +25,9 @@ char *	ft_strnstr(const char *haystack, const char *needle, size_t len)
 		j = 0;
 		while (haystack[i + j] == needle[j] && i + j < len)
 		{
-			j++;
-			if(needle[j] == '\0')
+			if(needle[j + 1] == '\0')
 				return ((char *)&haystack[i]);
+			j++;
 		}
 		i++;
 	}
